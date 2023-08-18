715b93264916a8fb19b1e22d3076997b7aab427e
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-18 19:37:24 +0400

    fixing airtable issue

--- api/index.mjs
+++ api/index.mjs
@@ -20,17 +20,37 @@ app.get('/', (req, res) => {
     res.send('Server Deployed 🥳')
 })
 
-app.get("/airtable", async (req, response) => {
-    await fetch(`https://api.airtable.com/v0/appd8UNly6GmJ3LFX/users`, {
-    })
-        .then((response) => response.json())
-        .then((result) => {
-            return response.json(result);
-        })
-        .catch((err) => {
-            return console.log(err);
+// app.get("/airtable", async (req, response) => {
+//     await fetch(`https://api.airtable.com/v0/appd8UNly6GmJ3LFX/users`, {
+//     })
+//         .then((response) => response.json())
+//         .then((result) => {
+//             return response.json(result);
+//         })
+//         .catch((err) => {
+//             return console.log(err);
+//         });
+
+// });
+
+app.get('/airtable', async (req, res) => {
+    const airtableUrl = `https://api.airtable.com/v0/${'appd8UNly6GmJ3LFX'}/users`;
+
+    try {
+        const response = await fetch(airtableUrl, {
+            headers: {
+                Authorization: `Bearer ${'REDACTED_AIRTABLE_TOKEN'}`,
+            },
         });
+
+        const data = await response.json();
+        return res.json(data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'An error occurred while fetching data from Airtable.' });
+    }
 });
+
 
 
 app.post('/verify-recaptcha', async (req, res) => {
