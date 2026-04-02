aca68df7d7ee7c78a882b1a1022e6e504efb5679
Kasper Peulen <kasperpeulen@gmail.com>
2026-04-03 01:47:55 +0700

    eval: 2026-04-02T18-39-20Z-345086f4

---
+++ src/components/team/Team.stories.jsx
@@ -0,0 +1,16 @@
+import Team from './Team';
+
+const meta = {
+  title: 'AI Generated/Medium/Team',
+  component: Team,
+};
+
+export default meta;
+
+export const Default = {};
+
+export const Fullscreen = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
