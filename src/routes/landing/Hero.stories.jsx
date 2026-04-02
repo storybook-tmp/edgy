829ab03efcc873615130b3e7329351e452a762a9
Kasper Peulen <kasperpeulen@gmail.com>
2026-04-02 23:31:08 +0700

    eval: 2026-04-02T16-27-32Z-fe8dba6e

---
+++ src/routes/landing/Hero.stories.jsx
@@ -0,0 +1,14 @@
+import Hero from './Hero';
+
+export default {
+  title: 'AI Generated/Complex/Hero',
+  component: Hero,
+};
+
+export const Default = {
+  args: {},
+};
+
+export const Display = {
+  args: {},
+};
