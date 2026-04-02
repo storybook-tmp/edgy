72ef4222938e726fe2ce6b3f07c42b0b1a598d6b
Kasper Peulen <kasperpeulen@gmail.com>
2026-04-03 01:45:07 +0700

    eval: 2026-04-02T18-39-20Z-58397fd9

---
+++ src/routes/landing/Stats.stories.jsx
@@ -0,0 +1,16 @@
+import Stats from './Stats';
+
+const meta = {
+  title: 'AI Generated/Medium/Stats',
+  component: Stats,
+};
+
+export default meta;
+
+export const Default = {};
+
+export const FullWidth = {
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
